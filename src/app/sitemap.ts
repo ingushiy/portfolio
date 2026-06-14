e40b2131a6b365fa7df16d9ab2bf440b7195377d
Джабраил Тангиев <macios@MacBook-Air-Dzabrail.local>
2026-06-15 01:31:37 +0300

    feat: add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://portfolio-three-phi-l1nqlenhzc.vercel.app/",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://portfolio-three-phi-l1nqlenhzc.vercel.app/portfolio",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://portfolio-three-phi-l1nqlenhzc.vercel.app/contacts",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+  ];
+}
